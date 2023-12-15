213f740b498fbd3f74251bf02232f5f30ed11ce3
nitin-pandita <nitin_207013@saitm.org>
2023-12-15 21:03:33 +0530

    List operations

---
+++ ArrayAndList/List.py
@@ -0,0 +1,20 @@
+l1 = [5, 2, "Assam", "Nitin","India"]
+
+#* Inserting an element
+
+l1.insert(2,"Kartik");
+
+
+# using the append method
+l1.append([9,10,11])
+
+# using the extend method
+l1.extend([20,54,21])
+
+
+# removing the element
+l1.remove(20)
+
+#pop functionality
+l1.pop()
+print(l1)
