e00cf111cd7d0a1d31ade4a566f0ca9d25f9a6ab
nitin-pandita <nitin_207013@saitm.org>
2023-12-19 18:45:51 +0530

    Project

---
+++ PythonSideProject/EmailValidator.py
@@ -0,0 +1,11 @@
+import re
+# mr.nitinpandita@gmail.com
+email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+
+user_email = input("Enter your Email...")
+
+if re.search(email_condition, user_email):
+    print("Right Email")
+
+else: 
+    print("Wrong Email")
